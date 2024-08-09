4f4e9277b34f0b1570cd28995334a461d0bfab56
YE-EUN <musiclove529@naver.com>
2024-08-09 13:55:09 +0900

    p11053 가장 긴 증가하는 부분수열 v1

---
+++ BOJ/src/class4/p11053_가장긴증가하는부분수열/v1/Main.java
@@ -0,0 +1,34 @@
+package class4.p11053_가장긴증가하는부분수열.v1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+        int max = 0;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}

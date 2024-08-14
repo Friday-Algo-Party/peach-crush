14cc7d8f74be6a5897dff2dc350a3810bb2b8d18
YE-EUN <musiclove529@naver.com>
2024-08-14 10:06:24 +0900

    p15650 N과 M 2 v1

---
+++ BOJ/src/class4/p15650_N과M2/v1/Main.java
@@ -0,0 +1,47 @@
+package class4.p15650_N과M2.v1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static boolean[] visited;
+    static int[] sequence;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        sequence = new int[M];
+
+        findSequence(1, 0);
+
+        System.out.print(sb);
+
+    }
+
+    static void findSequence(int idx, int depth) {
+        if(depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(sequence[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = idx; i <= N; i++) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    sequence[depth] = i;
+                    findSequence(i+1, depth+1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
